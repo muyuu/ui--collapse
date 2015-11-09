5f5f2f5db77ed586023ef3e3a9dbfa58292c9f42
muyuu <anticyborg@gmail.com>
2015-11-09 11:21:35 +0900

    remove "new" call

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-/*global Collapse*/
+/*global uiCollapse*/
 (function($, global) {
     "use strict";
-    var collapse01 = new Collapse();
+    var collapse01 = uiCollapse();
 })(jQuery, (this || 0).self || global);
